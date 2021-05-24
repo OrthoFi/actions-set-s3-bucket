b8c6f19f2e109356e513b396980216b2f09d797d
Peter Trueblood <Peter.Trueblood@orthofi.com>
2021-05-24 13:11:36 -0600

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import * as core from '@actions/core'
+
+async function run(): Promise<void> {
+  try {
+    const environment = core.getInput('environment-name')
+    let s3Bucket
+    switch (environment) {
+      case 'production':
+      case 'demo':
+      case 'risk':
+      case 'lithium':
+      case 'hydrogen':
+      case 'helium':
+        s3Bucket = 'orthofi-production-lambdas'
+        break
+      default:
+        s3Bucket = 'orthofi-ockers-lambdas'
+        break
+    }
+
+    core.setOutput('name', s3Bucket)
+    core.info(`Setting s3 bucket to ${s3Bucket}`)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
